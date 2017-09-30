1831bfb9d04a7bbb3568e44f1bbed3a07fa1da0d
Robom11 <Robom11@users.noreply.github.com>
2017-09-30 22:42:05 +0200

    Nested swipers in basic example

--- examples/components/Basic/index.js
+++ examples/components/Basic/index.js
@@ -5,12 +5,21 @@ import {
 } from 'react-native'
 import Swiper from 'react-native-swiper'
 
+const Dimensions = require('Dimensions');
+const window = Dimensions.get('window');
+
 var styles = {
   wrapper: {
+    backgroundColor: 'transparent'
+  },
+  innerSwiper: {
+    width: window.width,
+    height: window.height / 3,
+    marginTop: 60,
+    backgroundColor: '#92BBD9'
   },
   slide1: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#9DD6EB'
   },
@@ -33,14 +42,35 @@ var styles = {
   }
 }
 
-export default () => <Swiper style={styles.wrapper} showsButtons>
-  <View style={styles.slide1}>
-    <Text style={styles.text}>Hello Swiper</Text>
-  </View>
-  <View style={styles.slide2}>
-    <Text style={styles.text}>Beautiful</Text>
-  </View>
-  <View style={styles.slide3}>
-    <Text style={styles.text}>And simple</Text>
-  </View>
-</Swiper>
+export default () =>
+  <Swiper style={styles.wrapper} showsButtons>
+
+    <View style={styles.slide1}>
+      <View style={styles.innerSwiper}>
+          <Swiper style={styles.wrapper} showsButtons>
+
+            <View style={styles.slide1}>
+              <Text style={styles.text}>Hello Swiper</Text>
+            </View>
+
+            <View style={styles.slide2}>
+              <Text style={styles.text}>Beautiful</Text>
+            </View>
+
+            <View style={styles.slide3}>
+              <Text style={styles.text}>And simple</Text>
+          </View>
+
+          </Swiper>
+      </View>
+    </View>
+
+    <View style={styles.slide2}>
+      <Text style={styles.text}>Beautiful</Text>
+    </View>
+
+    <View style={styles.slide3}>
+      <Text style={styles.text}>And simple</Text>
+    </View>
+
+  </Swiper>
